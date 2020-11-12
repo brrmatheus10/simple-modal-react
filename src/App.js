f78e05cebae188dec6ca513b06718a93be7c1f13
brrmatheus10 <matheusro1@hotmail.com>
2020-11-12 15:11:27 -0300

    feat: simple react example for study purpose

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const mario = {
+    nome: 'Mario',
+    idade: '50 anos',
+    compras: [
+      { nome: 'notebook', preco: 1000 },
+      { nome: 'geladeira', preco: 2000 },
+    ],
+    ativa: false,
+  };
+  const maria = {
+    nome: 'Maria',
+    idade: '35 anos',
+    compras: [
+      { nome: 'notebook', preco: 3000 },
+      { nome: 'geladeira', preco: 4000 },
+    ],
+    ativa: true,
+  };
+  const jose = {
+    nome: 'Jose',
+    idade: '75 anos',
+    compras: [
+      { nome: 'notebook', preco: 6000 },
+      { nome: 'geladeira', preco: 7000 },
+    ],
+    ativa: false,
+  };
+
+  const clienteAtual = jose;
+
+  const totalCompras = clienteAtual.compras.map((item) => item.preco).reduce((a, b) => a + b);
+  console.log(totalCompras);
+
+  const estiloAtivo = {
+    color: 'greewn',
+    fontSize: '10px',
+    fontFamily: 'Helvetica',
+  };
+  const estiloNegativo = {
+    color: 'red',
+    fontSize: '15px',
+    fontFamily: 'Helvetica',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Nome: {clienteAtual.nome}</p>
+      <p>Idade: {clienteAtual.idade}</p>
+      <p>
+        {' '}
+        Situação:
+        <span style={{ color: clienteAtual.ativa ? 'green' : 'red' }}> {clienteAtual.ativa ? 'Ativa' : 'inativa'}</span>
+      </p>
+      <p>Valor total das compras: {totalCompras}</p>
+      <p>{totalCompras > 5000 ? <p>Você passou do limite</p> : <p>Voce esta dentro do limite</p>}</p>
+    </>
   );
-}
+};
 
 export default App;
